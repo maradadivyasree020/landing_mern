34db51fe59e57a84dbc7ccf5882f59f2a31b2ce2
Marada Divya Sree <maradadivyasree@gmail.com>
2024-06-05 21:59:39 +0530

    pictures added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,7 @@
 import { Carousel } from 'flowbite-react'
 import React from 'react'
+import image from '../assets/image.png';
+import img11 from '../assets/img11.jpg';
 import '../App.css'
 
 const Home = () => {
@@ -15,16 +17,16 @@ const Home = () => {
                 </div>
             </div>
             <div className=" my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
-                <div><img src="https://media.istockphoto.com/id/1248915720/photo/farmers-hand-watering-a-young-plant.jpg?s=612x612&w=0&k=20&c=kip26_08vy0zT90x2bA9frWUD6ZEuzPkw8_9uv8cfrw=" className='w-[800px] h-[500px]'/></div>
+                <div><img src={image} className='w-[800px] h-[500px]'/></div>
                 <div className='md:w-1/2'>
-                    <h1 className='text-5xl font-semibold mb-4 text-neutralDGrey md:w-3/4 leading-snug'>"Planting trees is planting hope."<br/><span className=' text-xl text-brandPrimary leading-snug'>— Wangari Maathai</span></h1>
+                    <h1 className='text-5xl font-semibold mb-4 text-neutralDGrey md:w-3/4 leading-snug'>"There are more life forms in a handful of forest soil than there are people on the planet."<br/><span className=' text-xl text-brandPrimary leading-snug'>— Peter Wohlleben</span></h1>
                     <button className='px-7 py-2 bg-brandPrimary text-white rounded hover:bg-neutralDGrey transition-all duration-300 hover:-translate-y-4'>Register</button>
                 </div>
             </div>
             <div className=" my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
-                <div><img src="https://media.istockphoto.com/id/1248915720/photo/farmers-hand-watering-a-young-plant.jpg?s=612x612&w=0&k=20&c=kip26_08vy0zT90x2bA9frWUD6ZEuzPkw8_9uv8cfrw=" className='w-[800px] h-[500px]'/></div>
+                <div><img src={img11} className='h-[500px]'/></div>
                 <div className='md:w-1/2'>
-                    <h1 className='text-5xl font-semibold mb-4 text-neutralDGrey md:w-3/4 leading-snug'>"Planting trees is planting hope."<br/><span className=' text-xl text-brandPrimary leading-snug'>— Wangari Maathai</span></h1>
+                    <h1 className='text-5xl font-semibold mb-4 text-neutralDGrey md:w-3/4 leading-snug'>"He who plants a tree plants a hope"<br/><span className=' text-xl text-brandPrimary leading-snug'>-Lucy Larcom</span></h1>
                     <button className='px-7 py-2 bg-brandPrimary text-white rounded hover:bg-neutralDGrey transition-all duration-300 hover:-translate-y-4'>Register</button>
                 </div>
             </div>
